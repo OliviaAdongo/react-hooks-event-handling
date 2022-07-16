aba9f6ab22861ab572f258e40768b514db9058b4
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-17 01:56:42 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,13 @@ import MultiButton from "./MultiButton";
 import ChangeItUp from "./ChangeItUp";
 import Login from "./Login";
 
+function Clickable() {
+  function handleClick() {
+    console.log("click");
+  }
+  return <button onClick={handleClick}>Click Me</button>;
+}
+
 function App() {
   return (
     <div>
@@ -21,6 +28,8 @@ function App() {
       <h3>onSubmit</h3>
       <Login />
       <hr />
+      <h3>clicked</h3>
+      <Clickable/>
     </div>
   );
 }
